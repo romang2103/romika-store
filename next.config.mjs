082ae3695efcd8a3f1530c81b5557e51188a10ef
Roman Gurung <romang2103@gmail.com>
2024-12-18 04:53:29 +0000

    Add google storage api into config for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+        domains: ['storage.googleapis.com'],
+      },
 };
 
 export default nextConfig;
