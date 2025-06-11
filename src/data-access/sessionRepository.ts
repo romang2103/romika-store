45ec443173e8a337306a0a5035f89524f80e449a
Roman Gurung <romang2103@gmail.com>
2025-06-11 20:08:21 +0100

    Implement expiration and sliding expiration logic

--- src/data-access/sessionRepository.ts
+++ src/data-access/sessionRepository.ts
@@ -2,15 +2,16 @@
 
 import clientPromise from "@/lib/mongodb";
 import { SessionData } from "@/interfaces/interfaces";
+import { Session } from "inspector";
 
-export async function createSession(sessionId: string): Promise<SessionData> {
+export async function createSession(sessionData: SessionData): Promise<SessionData> {
     const client = await clientPromise;
     const db = client.db('romika-db');
     const collection = db.collection('sessions');
-    const newSession = { sessionId };
-    await collection.insertOne(newSession);
+    const { _id, ...newSessionData } = sessionData; // Exclude _id
+    const result = await collection.insertOne({ ...newSessionData });
     console.log('Session created');
-    return newSession;
+    return { _id: result.insertedId.toHexString(), ...newSessionData };
 }
 
 export async function getSession(sessionId: string): Promise<SessionData | null> {
@@ -29,9 +30,20 @@ export async function getSession(sessionId: string): Promise<SessionData | null>
     return null;
 }
 
-// export async function updateSessionCart(sessionId: string, cart: any[]): Promise<void> {
-//     const client = await clientPromise;
-//     const db = client.db('romika-db');
-//     const collection = db.collection('sessions');
-//     await collection.updateOne({ sessionId }, { $set: { cart } });
-// }
+export async function extendSession(sessionId: string, newExpiry: Date, lastActive: Date): Promise<void> {
+    const client = await clientPromise;
+    const db = client.db("romika-db");
+    const collection = db.collection("sessions");
+
+    await collection.updateOne(
+        { sessionId },
+        { $set: { expiresAt: newExpiry, lastActiveAt: lastActive } }
+    );
+}
+
+export async function deleteSession(sessionId: string): Promise<void> {
+    const client = await clientPromise;
+    const db = client.db("romika-db");
+    const collection = db.collection("sessions");
+    await collection.deleteOne({ sessionId });
+}
